e75dbe4ed9ff457c9b83ee8fed7c6d68e088fb15
hhuizhang <cathyzh8807@gmail.com>
2014-04-17 22:42:26 +0800

    add Logger class

--- src/chapter5/ObjectOrientedDesignPrinciples/Test.java
+++ src/chapter5/ObjectOrientedDesignPrinciples/Test.java
@@ -2,14 +2,27 @@ package chapter5.ObjectOrientedDesignPrinciples;
 
 public class Test {
       
-	public static void main(String[] args){
+//	public static void main(String[] args){
+//		
+//		Circle circle = new Circle(10,20,20);
+//		System.out.println(circle);
+//		circle.addObserver(new ShapeAchiver());
+//		circle.addObserver(new Canvas());
+//		circle.setCenter(new Point(12,22));
+//		circle.setRadius(50);
+//		System.out.println(circle);
 		
-		Circle circle = new Circle(10,20,20);
-		System.out.println(circle);
-		circle.addObserver(new ShapeAchiver());
-		circle.addObserver(new Canvas());
-		circle.setCenter(new Point(12,22));
-		circle.setRadius(50);
-		System.out.println(circle);
+		public static void main(String[] args){
+			Logger loggerInstance1;
+			loggerInstance1 = Logger.getInstance();
+			System.out.println(loggerInstance1);
+			Logger loggerInstance2;
+			loggerInstance2 = Logger.getInstance();
+			System.out.println(loggerInstance2);
+			Logger loggerInstance3;
+			loggerInstance3 = Logger.getInstance();
+			System.out.println(loggerInstance3);
+
+			
 	}
 }
