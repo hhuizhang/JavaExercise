42b77a185c7f1d196fbe999c230cc3ce00c6c02c
catherine.zh <catherine.zh@koubei.com>
2019-04-01 17:44:55 +0800

    add MyTest2 MyTest3

---
+++ src/main/java/coreJava/MyTest2.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc. Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package coreJava;
+
+/**
+ *
+ * @author catherine
+ * @version $Id: MyTest2.java, v 0.1 2019年03月29日 18:23 catherine Exp $
+ */
+public class MyTest2 {
+}
