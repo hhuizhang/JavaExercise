94ecd42b1caab71b098d58e9fcbb748314208ffd
catherine.zh <catherine.zh@koubei.com>
2019-04-02 13:08:50 +0800

    222

---
+++ src/main/java/coreJava/interfaces/MyTest4.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc. Copyright (c) 2004-2019 All Rights Reserved.
+ */
+package coreJava.interfaces;
+
+/**
+ *
+ * @author catherine
+ * @version $Id: MyTest4.java, v 0.1 2019年04月02日 13:06 catherine Exp $
+ */
+public class MyTest4 {
+}
