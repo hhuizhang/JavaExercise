2a5f0033d07fe2fd10f05067091d9849a48b1881
catherine.zh <catherine.zh@koubei.com>
2019-04-03 15:50:32 +0800

    update Mytest5

--- src/main/java/coreJava/MyTest5.java
+++ src/main/java/coreJava/MyTest5.java
@@ -9,4 +9,8 @@ package coreJava;
  * @version $Id: MyTest5.java, v 0.1 2019年04月03日 15:40 catherine Exp $
  */
 public class MyTest5 {
+
+    public static void main(String[] args) {
+        System.out.println("This is dev-2010328-2");
+    }
 }
